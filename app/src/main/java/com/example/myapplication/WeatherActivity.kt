6d800189c47038b97b4b999f024687aa851aa9cb
Krisz-banki <77264324+Krisz-banki@users.noreply.github.com>
2024-12-09 10:17:09 +0100

    Weather Update

--- app/src/main/java/com/example/myapplication/WeatherActivity.kt
+++ app/src/main/java/com/example/myapplication/WeatherActivity.kt
@@ -2,7 +2,10 @@ package com.example.myapplication
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.model.WeatherResponse
@@ -17,7 +20,13 @@ class WeatherActivity : AppCompatActivity() {
 //komment
     private lateinit var textviewTemp: TextView
     private lateinit var textviewTempmin: TextView
+    private lateinit var textviewHumidity: TextView
+    private lateinit var textviewWindSpeed: TextView
+    private lateinit var weatherButton: Button
+    private lateinit var EditTextCity: EditText
+    private lateinit var resultTextView: TextView
     private val apiKey = "0db176500d087d078df1c33397cc1aa0"
+    private var getCity = "";
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +35,22 @@ class WeatherActivity : AppCompatActivity() {
 
         textviewTemp = findViewById(R.id.textview_temp)
         textviewTempmin = findViewById(R.id.textview_tempmin)
+        textviewHumidity = findViewById(R.id.textview_humidity)
+        textviewWindSpeed = findViewById(R.id.textview_windSpeed)
+        weatherButton = findViewById(R.id.weatherButton)
+        EditTextCity = findViewById(R.id.EditTextCity)
+        resultTextView = findViewById(R.id.resultTextView)
+
+        weatherButton.setOnClickListener{
+            val cityName = EditTextCity.text
+
+            if (cityName.isNotBlank()) {
+                getCity = cityName.toString()
+            } else {
+                Toast.makeText(this, "Adj meg egy várost!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
         fetchWeatherData()
     }
 
@@ -37,7 +62,7 @@ class WeatherActivity : AppCompatActivity() {
 
         val weatherService = retrofit.create(WeatherService::class.java)
 
-        val call = weatherService.getWeather("Tatabánya","metric", apiKey)
+        val call = weatherService.getWeather(getCity,"metric", apiKey)
 
         call.enqueue(object: Callback<WeatherResponse> {
             override fun onResponse(
@@ -51,6 +76,10 @@ class WeatherActivity : AppCompatActivity() {
                         textviewTemp.text = weatherInfo.toString()
                         val weatherInfoMin = weatherResponse.main.feels_like
                         textviewTempmin.text = weatherInfoMin.toString()
+                        val weatherInfoHumidity = weatherResponse.main.humidity
+                        textviewHumidity.text = weatherInfoHumidity.toString()
+                        val weatherInfoWindSpeed = weatherResponse.wind.speed
+                        textviewWindSpeed.text = weatherInfoWindSpeed.toString()
                     }
                 }
             }
--- app/src/main/java/com/example/myapplication/model/WeatherResponse.kt
+++ app/src/main/java/com/example/myapplication/model/WeatherResponse.kt
@@ -1,10 +1,15 @@
 package com.example.myapplication.model
 
 data class WeatherResponse(
-    val main: Main
+    val main: Main,
+    val wind: Wind
 )
 
 data class Main(
     val temp: Float,
-    val feels_like: Float
+    val feels_like: Float,
+    val humidity: Integer
+)
+data class Wind(
+    val speed: Float
 )
